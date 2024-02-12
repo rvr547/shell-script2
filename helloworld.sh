f0806859e46e0c8e94f4adf40c6296997e75ec3e
Vandana <rikkavandanareddy@gmail.com>
2024-02-12 15:10:01 +0530

    hello world

--- helloworld.sh
+++ helloworld.sh
@@ -2,7 +2,9 @@
 
 echo "Hello World"
 
-echo " abc: hello xyz, good morning"
-echo " xyz: hello abc, good morning"
-echo " abc: hello xyz, How are you ?"
-echo " xyz: hello xyz, I am good"
+PERSON1=Ramesh
+PERSON2=xyz
+echo " $Person1: hello $Person2, good morning"
+echo " $Person2: hello $Person1, good morning"
+echo " $Person1: hello $Person2, How are you ?"
+echo " $Person2: hello $Person1, I am good"
