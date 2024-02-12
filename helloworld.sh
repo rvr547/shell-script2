b18f28f5441c2f01d990ffa15f4dfb64ea946b42
Vandana <rikkavandanareddy@gmail.com>
2024-02-12 15:06:33 +0530

    hello world

--- helloworld.sh
+++ helloworld.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Hello World"
+
+echo " abc: hello xyz, good morning"
+echo " xyz: hello abc, good morning"
+echo " abc: hello xyz, How are you ?"
+echo " xyz: hello xyz, I am good"
