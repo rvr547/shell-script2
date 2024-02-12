041b850fecf8f7760f22b13c8812d9ddc758d087
Vandana <rikkavandanareddy@gmail.com>
2024-02-12 15:16:38 +0530

    hello world

---
+++ dynamicinput.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+echo " $PERSON1: hello $PERSON2, good morning"
+echo " $PERSON2: hello $PERSON1, good morning"
+echo " $PERSON1: hello $PERSON2, How are you ?"
+echo " $PERSON2: hello $PERSON1, I am good"
