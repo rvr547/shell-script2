719b7096d54e81f827fd92af749f7fbec21dbcea
Vandana <rikkavandanareddy@gmail.com>
2024-02-12 18:08:55 +0530

    installation

--- install.sh
+++ install.sh
@@ -6,4 +6,12 @@ then
     echo "Error: Please run this as root user"
     exit 1
 fi
+#to check installation is success or not
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation unsuccessful"
+else
+    echo "Installation successful"
+fi
