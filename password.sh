32820f586ca00947327b9d677c79e9502beded32
Vandana <rikkavandanareddy@gmail.com>
2024-02-12 15:25:02 +0530

    hello world

---
+++ password.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#to read password from command line in linux
+
+echo "Please enter Username:"
+read USERNAME
+
+echo "User name is : $USERNAME"
+
+echo "Please enter password:"
+read PASSWORD
+
+echo "Password  is : $PASSWORD"
