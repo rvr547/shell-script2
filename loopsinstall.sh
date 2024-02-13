d64e9d1a023e7c8e654223590ad2963d01e27135
Vandana <rikkavandanareddy@gmail.com>
2024-02-13 21:33:52 +0530

    Loops

--- loopsinstall.sh
+++ loopsinstall.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 #add improvemnts
-
-for i in $@
-do
-    yum install $i -y
-done
+USER=$(id -u)
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+R="\e[31m"
+G="\e[32m"
+N="\2[0m"
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R unsuccessful $N"
+    else
+        echo -e "$2 ... $G successful $N"
+    fi
+}
+if [ $USER -ne 0 ]
+then
+    echo -e "ERROR: $R USE Root access to use this command $N"
+else
+    for i in $@
+    do
+        yum install $i -y &>>$LOGFILE
+        VALIDATE $? "Installation of $i"
+    done
+fi
