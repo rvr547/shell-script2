4fca7a462b3b9eef62c90fc7463fbe09aa1556c7
Vandana <rikkavandanareddy@gmail.com>
2024-02-14 16:11:11 +0530

    delete log

--- deleteoldlogs.sh
+++ deleteoldlogs.sh
@@ -5,3 +5,8 @@ SCRIPT_NAME=$0
 LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +15)
 echo " $FILES_TO_DELETE "
+
+while read line
+do
+    echo "Deleting $line " &>> $LOGFILE
+done <<< FILES_TO_DELETE
