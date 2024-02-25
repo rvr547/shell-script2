d571516058a8596a58c2052140925478d0504bb5
Vandana <rikkavandanareddy@gmail.com>
2024-02-25 16:53:43 +0530

    ceate server

--- create-servers.sh
+++ create-servers.sh
@@ -16,7 +16,7 @@ do
 echo "Creating instance $i"
 IP_ADDRESS=$(aws ec2 run-instances --image-id $IMGID  --instance-type $INSTANCE_TYPE --security-group-ids $SEC_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" |  jq -r '.Instances[0].PrivateIpAddress')
 echo "IP address of instance $i: $IP_ADDRESS"
-aws route53 change-resource-record-sets --hosted-zone-id Z05567796ESR36J2OFN7 --security-group-ids $SEC_GROUP_ID --change-batch '
+aws route53 change-resource-record-sets --hosted-zone-id Z05567796ESR36J2OFN7 --change-batch '
 {
         "Comment": "CREATE/DELETE/UPSERT a record ",
                     "Changes": [{
