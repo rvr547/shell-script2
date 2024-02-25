ae3b99907740af48baa9c62cd2d3c1d83332b8c5
Vandana <rikkavandanareddy@gmail.com>
2024-02-25 16:10:16 +0530

    ceate server

--- create-servers.sh
+++ create-servers.sh
@@ -7,12 +7,12 @@ DOMAIN_NAME=preprv.online.com
 
 for i in $@
 do
-if [[ $i == "mongodb" || $i == "mysql"]]
-then
-    INSTANCE_TYPE="t3.micro"
-else
-    INSTANCE_TYPE="t2.micro"
-fi
+    if [[ $i == "mongodb" || $i == "mysql" ]]
+    then
+        INSTANCE_TYPE="t3.micro"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
 echo "Creating instance $i"
 IP_ADDRESS=$(aws ec2 run-instances --image-id $IMGID  --instance-type $INSTANCE_TYPE --security-group-ids $SEC_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" |  jq -r '.Instances[0].PrivateIpAddress')
 echo "IP address of instance $i: $IP_ADDRESS"
